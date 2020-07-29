39e5fb92df76849f62914d2796b04193174c1a30
Philip Warren <philwarren1@gmail.com>
2020-07-29 20:06:15 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -22,18 +22,11 @@ def adding
   shipping_manifest = {
                   "whale bone corsets" => 5, 
                   "porcelain vases" => 2, 
-                  "oil paintings" => 3, 
-                  "muskets" => 2
-                  "gun powder" => 4
+                  "oil paintings" => 3,
                  }
+  shipping_manifest["muskets"] = 2
+  shipping_manifest["gun powder"] = 4
+shipping_manifest
 
-
-
-   shipping_manifest
-  # add 4 gun powder to the shipping_manifest hash below
-
-
-
-  # return the shipping_manifest hash below
 
 end
