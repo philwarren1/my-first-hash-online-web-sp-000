866af0126f389fd63dc1a2db894f9f6dc2394578
Philip Warren <philwarren1@gmail.com>
2020-07-29 19:56:49 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -14,7 +14,7 @@ def retrieval
                   "porcelain vases" => 2, 
                   "oil paintings" => 3
                  }
-    shipping_manifest[2] 
+    return shipping_manifest[2] 
 end    
 
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
