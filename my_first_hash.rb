295417e94b85e1f602dcbe5cf9e473d4bb485931
Philip Warren <philwarren1@gmail.com>
2020-07-29 19:56:28 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -19,8 +19,6 @@ end
 
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
 
-end
-
 def adding
   shipping_manifest = {
                   "whale bone corsets" => 5, 
