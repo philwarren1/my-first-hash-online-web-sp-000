8c1f02133fdbc6ab68813de4ae2ba0c6cc0703c4
Philip Warren <philwarren1@gmail.com>
2020-07-29 20:04:50 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -17,18 +17,19 @@ def retrieval
     shipping_manifest["oil paintings"] 
 end    
 
-  # Look up the value of the "oil paintings" key in the shipping_manifest hash below
 
 def adding
   shipping_manifest = {
                   "whale bone corsets" => 5, 
                   "porcelain vases" => 2, 
-                  "oil paintings" => 3
+                  "oil paintings" => 3, 
+                  "muskets" => 2
+                  "gun powder" => 4
                  }
-  # add 2 muskets to the shipping_manifest hash below
 
 
 
+  return shipping_manifest
   # add 4 gun powder to the shipping_manifest hash below
 
 
